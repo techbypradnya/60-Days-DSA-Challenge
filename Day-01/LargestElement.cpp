492fe95bfe7657bfb99e96629a3346692294b418
pradnya-001 <pradnyapangavhane23@gmail.com>
2026-03-21 17:44:39 +0530

    Added solution for largest element

---
+++ Day-01/LargestElement.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int largestElement(int arr[], int n)
+{
+    int max = arr[0];
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+    int arr[n];
+
+    cout << "Enter the elements of the array : ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int result = largestElement(arr, n);
+
+    cout << " the largest Element in the array is:" << result << endl;
+    return 0;
+}
