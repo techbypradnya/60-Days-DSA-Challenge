7ebeeef9b1ea6a29c4e9c5f2da227f5bd55f17c3
pradnya-001 <pradnyapangavhane23@gmail.com>
2026-03-22 16:00:49 +0530

    message

---
+++ ArrayReversing.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+ using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter the size of the array : ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter the elements of the array :";
+    for(int i = 0;i<n; i++){
+        cin >> arr[i];
+    }
+    return 0;
+}
