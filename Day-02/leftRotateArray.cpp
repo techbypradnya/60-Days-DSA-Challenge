590986ccf8fd6a930c5f235b18eafe97f5cae29e
pradnya-001 <pradnyapangavhane23@gmail.com>
2026-03-22 19:57:48 +0530

    message

--- Day-02/leftRotateArray.cpp
+++ Day-02/leftRotateArray.cpp
@@ -2,7 +2,7 @@
  using namespace std;
 
  int LeftRotateArray(int arr[],int n){
-    
+
  }
 
 int main() {
@@ -14,6 +14,12 @@ int main() {
    cout << " Enter the elements of the array :";
     for(int i =0;i< n ; i++){
         cin >> arr[i];
+    }
+
+    cout << "After left rotation: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
     }
    
     return 0;
