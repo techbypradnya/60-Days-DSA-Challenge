bb45074e6a5d9571778d6c19e7a2021a969083fe
pradnya-001 <pradnyapangavhane23@gmail.com>
2026-03-22 19:56:39 +0530

    message

--- Day-02/leftRotateArray.cpp
+++ Day-02/leftRotateArray.cpp
@@ -6,5 +6,11 @@ int main() {
    cout << "Enter the size of the array : ";
    cin >> n;
    int arr[n];
+
+   cout << " Enter the elements of the array :";
+    for(int i =0;i< n ; i++){
+        cin >> arr[i];
+    }
+   
     return 0;
 }
