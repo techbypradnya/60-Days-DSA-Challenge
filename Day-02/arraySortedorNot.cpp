b7d10119b5a072021cca32af2133e499e6ea7e26
pradnya-001 <pradnyapangavhane23@gmail.com>
2026-03-22 15:59:15 +0530

    message

--- Day-02/arraySortedorNot.cpp
+++ Day-02/arraySortedorNot.cpp
@@ -1,14 +1,37 @@
 #include <iostream>
- using namespace std;
+using namespace std;
 
-int main() {
+int arraySorted(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (arr[i] > arr[i + 1])
+        {
+            return false;
+        }
+        }
+    return true;
+}
+
+int main()
+{
     int n;
-    cout << " eter the size of the array : ";
+    cout << " Enter the size of the array : ";
     cin >> n;
     int arr[n];
     cout << " Enter the elements of the array :";
-    for(int i = 0;i<n;i++){
+    for (int i = 0; i < n; i++)
+    {
         cin >> arr[i];
+    }
+
+    if (arraySorted(arr, n))
+    {
+        cout << "Array is Sorted" << endl;
+    }
+    else
+    {
+        cout << "Array is not sorted" << endl;
     }
     return 0;
 }
