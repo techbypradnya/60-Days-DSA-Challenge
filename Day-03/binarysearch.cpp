298a918d194a840029b03f24923f578d8052b5c9
pradnya-001 <pradnyapangavhane23@gmail.com>
2026-03-23 19:06:45 +0530

    message

--- Day-03/binarysearch.cpp
+++ Day-03/binarysearch.cpp
@@ -2,6 +2,24 @@
  using namespace std;
 
 int main() {
-    
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    int key;
+    cout << "Enter the key to search: ";
+    cin >> key;
+
+    int result = linearSearch(arr, n, key);
+    if(result != -1){
+        cout << "Element found at index: " << result << endl;
+    } else {
+        cout << "Element not found in the array." << endl;
+    }
+
     return 0;
 }
