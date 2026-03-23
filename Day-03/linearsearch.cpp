5ba84b7537a5d9441b087851f52b9b873a774ef1
pradnya-001 <pradnyapangavhane23@gmail.com>
2026-03-23 19:04:04 +0530

    message

--- Day-03/linearsearch.cpp
+++ Day-03/linearsearch.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
  using namespace std;
 
- void linearSearch(int arr[], int n,int key){
+ int linearSearch(int arr[], int n,int key){
     for(int i = 0;i < n;i++){
         if(arr[i] == key){
             return i;
