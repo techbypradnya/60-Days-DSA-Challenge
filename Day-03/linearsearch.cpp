5a13151bb97cc116bd6cd266908e994bbe608ffe
pradnya-001 <pradnyapangavhane23@gmail.com>
2026-03-23 18:52:58 +0530

    message

--- Day-03/linearsearch.cpp
+++ Day-03/linearsearch.cpp
@@ -2,6 +2,17 @@
  using namespace std;
 
 int main() {
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    int key;
+    cout << "Enter the key to search: ";
+    cin >> key;
     
     return 0;
 }
