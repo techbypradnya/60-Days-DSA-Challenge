3158bcd9bb01a7f8224b9f408a35568f8e8a5b75
pradnya-001 <pradnyapangavhane23@gmail.com>
2026-03-24 11:38:57 +0530

    message

---
+++ Day-04/findMissingValues.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+ using namespace std;
+
+
+int FindMisssingValues(int arr[], int n){
+    
+}
+
+int main() {
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+    int arr[n];
+    cout <<"Enter the elements of the array: ";
+    for(int i =0 ;i <n;i++){
+        cin >> arr[i];
+    }
+
+    return 0;
+}
