eff3dd3586a7881174a4ff4828101c04bca1fa3b
pradnya-001 <pradnyapangavhane23@gmail.com>
2026-03-24 10:57:06 +0530

    message

--- Day-04/removingDuplicates.cpp
+++ Day-04/removingDuplicates.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
- using namespace std;
+using namespace std;
+
+int removeDuplicates(int arr[], int n) {
+    // Edge case
+    if (n == 0) return 0;
+
+    int i = 0; // pointer for unique elements
+
+    for (int j = 1; j < n; j++) {
+        if (arr[j] != arr[i]) {
+            i++;
+            arr[i] = arr[j];
+        }
+    }
+
+    return i + 1; // new size of array
+}
 
 int main() {
-    
+    int arr[] = {1, 1, 2, 2, 3, 3, 4};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    int newSize = removeDuplicates(arr, n);
+
+    cout << "Array after removing duplicates: ";
+    for (int i = 0; i < newSize; i++) {
+        cout << arr[i] << " ";
+    }
+
+    cout << "\nNumber of unique elements: " << newSize;
+
     return 0;
 }
